496a79f522cf82c6b4fe274b194f6e21b049c788
riokt <rizqi.okta@gmail.com>
2018-04-04 13:55:15 +0700

    nambah hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+class Rocket():
+    # Rocket simulates a rocket ship for a game,
+    #  or a physics simulation.
+    
+    def __init__(self, x=0, y=0):
+        # Each rocket has an (x,y) position.
+        self.x = x
+        self.y = y
+        
+    def move_rocket(self, x_increment=0, y_increment=1):
+        # Move the rocket according to the paremeters given.
+        #  Default behavior is to move the rocket up one unit.
+        self.x += x_increment
+        self.y += y_increment
